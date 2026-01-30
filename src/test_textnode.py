04385d02cd481db9bf64be188051cfb27cfc369c
Henry Helbock <hjhelbock@gmail.com>
2026-01-30 12:51:06 -0500

    Updated textnodes for conversions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
-
+from textnode import TextNode, TextType, text_node_to_html_node
+from delimiters import split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -21,6 +21,64 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertIsInstance(node.text, str)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+
+    def test_no_delimiter(self):
+        nodes = [TextNode("Just plain text", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+        self.assertEqual(result, nodes)
+
+    def test_single_code_delimiter(self):
+        nodes = [TextNode("This is `code` text", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+        self.assertEqual(result, [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" text", TextType.TEXT),
+        ])
+
+    def test_multiple_code_delimiters(self):
+        nodes = [TextNode("`a` and `b`", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+        self.assertEqual(result, [
+            TextNode("a", TextType.CODE),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("b", TextType.CODE),
+        ])
+
+    def test_non_text_nodes_unchanged(self):
+        nodes = [
+            TextNode("Hello ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" world", TextType.TEXT),
+        ]
+
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+        self.assertEqual(result, nodes)
+
+    def test_unmatched_delimiter_raises(self):
+        nodes = [TextNode("This is `broken text", TextType.TEXT)]
+
+        with self.assertRaises(Exception):
+            split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+    def test_empty_segments_are_skipped(self):
+        nodes = [TextNode("``code``", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+        self.assertEqual(result, [
+            TextNode("code", TextType.CODE),
+        ])
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
     TEXT = "text"
     BOLD = "BOLD"
@@ -24,5 +26,34 @@ class TextNode():
     
     def __repr__(self):
         return(f"TextNode({self.text}, {self.text_type.value}, {self.url})")
+    
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node.text_type, TextType):
+        raise Exception("not an instance of text type")
+    else:
+        if text_node.text_type == TextType.TEXT:
+            node = LeafNode(None, text_node.text)
+            return node
 
+        if text_node.text_type == TextType.BOLD:
+            node = LeafNode("b", text_node.text)
+            return node
+
+        if text_node.text_type == TextType.ITALIC:
+            node = LeafNode("i", text_node.text)
+            return node
+        
+        if text_node.text_type == TextType.CODE:
+            node = LeafNode("code", text_node.text)
+            return node
+        
+        if text_node.text_type == TextType.LINK:
+            node = LeafNode("a", text_node.text, props={"href" : text_node.url})
+            return node
+        
+        if text_node.text_type == TextType.IMAGE:
+            node = LeafNode("img", "", props={"src" : text_node.url, "alt" : text_node.text})
+            return node
+            
+            
 
